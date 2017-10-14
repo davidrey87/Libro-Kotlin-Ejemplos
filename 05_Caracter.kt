9b9e817cc4adb2b474641f3bc2ecc959ba36e20e
David REyes <isc.redh@gmail.com>
2017-10-14 01:19:17 -0500

    Se agregan ejercicios del 5 al 6

---
+++ 05_Caracter.kt
@@ -0,0 +1,5 @@
+//Usando Chars
+fun main(args: Array<String>) {
+    var caracter: Char = '\n'//Salto de linea
+    print("Hola $caracter Mundo $caracter")
+}
---
+++ 06_Boleano.kt
@@ -0,0 +1,7 @@
+//Inicializando variable boleana
+fun main(args: Array<String>) {
+    val a = 3
+    val b = 3
+    val esIgual: Boolean = (a == b)
+    println(esIgual)
+}
